7f60ce1dc97329f4c94c9a96caef2c52762f2471
Raihan Fathurrahman <raihan25507@gmail.com>
2025-12-27 01:04:20 +0700

    Fix storage path for Vercel3

--- public/index.php
+++ public/index.php
@@ -1,20 +1,25 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// 1. Cek Maintenance Mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// 2. Register Composer
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// 3. Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+/* --- JURUS SAKTI VERCEL ---
+   Memaksa Laravel menggunakan folder /tmp untuk penyimpanan 
+   karena folder lain Read-Only.
+*/
+$app->useStoragePath('/tmp/storage');
+
+// 4. Handle Request
 $app->handleRequest(Request::capture());
