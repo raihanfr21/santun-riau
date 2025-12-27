477865e4f3bac71743c1d56fa48ec233d79e3662
Raihan Fathurrahman <154324482+raihanfr21@users.noreply.github.com>
2025-12-27 07:37:32 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\AuthController; // <--- INI KUNCI PERBAIKANNYA
 
 // Halaman Depan
 Route::get('/', [ReportController::class, 'index'])->name('home');
+Route::get('/lapor', [ReportController::class, 'index'])->name('lapor.index');
 Route::post('/lapor', [ReportController::class, 'store'])->name('lapor.store');
 
 // --- ROUTE AUTH (LOGIN/LOGOUT) ---
